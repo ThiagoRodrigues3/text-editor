2ba4323671ad4a0c6eb6e4649a755d522c641310
Thiago Rod <whytigo@gmail.com>
2022-03-14 10:25:18 -0400

    added all the Client src JS structure for the DB and Install

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the jate database');
+  // DB set up and queries
+  const jateDb = await openDB('jate',1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // Update
+  const request = store.put({ id: 1, text: content});
+  const result = await request;
+  console.log("Text Updated");
+  return result;
+};
+
+
+export const getDb = async () => {
+  console.log('PUT to the jate database');
+  // DB set up and queries
+  const jateDb = await openDB('jate',1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  // Get
+  const request = store.get(1);
+  const result = await request;
+  console.log("Text has been retrieved.");
+  return result.text;
+};
 
 initdb();
